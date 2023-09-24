ec7f160cd3ecfbe90cace2cc4b0a07f97b292d1b
Gustavo Rodrigues <87579597+duguzin@users.noreply.github.com>
2023-09-24 19:30:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 const toggleMenuOpen = () => document.body.classList.toggle("open");
-
-// var counter = 1;
-// setInterval(function(){
-//   document.getElementById('radio' + counter).checked = true;
-//   counter++;
-//   if (counter > 3) {
-//     counter = 1;
-//   }
-// }, 5000);
